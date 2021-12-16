77a450d3a10214ed080a4f47b11a1e5a65fe8d79
Manoj Pedvi <manoj.pedvi@gmail.com>
2021-12-16 14:03:34 +0530

    first commit

---
+++ data/profile-dummy.js
@@ -0,0 +1,161 @@
+const profileData = {
+  title: 'Profile',
+  name: 'Manoj Pedvi',
+  sub_title: 'Sr. Android Engineer',
+  about: {
+    intro: ``,
+    contact: {
+      email: 'manoj.pedvi.com',
+      phone: '+91-9850347122',
+      address: 'Mumbai, Maharashtra'
+    }
+  },
+  links: [
+    { title: 'LinkedIn', src: 'https://in.linkedin.com/in/manoj-pedvi' },
+  ],
+  education: [
+    {
+      alma: 'Graduating May 2011',
+      duration: '',
+      std: '',
+      score: 'Mumbai University'
+    }
+  ],
+  skills: [
+    {
+      category: 'Mobile Technologies',
+      topics: ['Android', 'Xamarin', 'React-Native']
+    },
+    {
+      category: 'Programming Languages',
+      topics: ['Kotlin', 'Java', 'C#', 'Node']
+    },
+    {
+      category: 'Databases',
+      topics: ['SQLite', 'SQL Server', 'My SQL', 'Realm']
+    },
+    {
+      category: 'Web technologies:',
+      topics: [
+        'HTML', 'CSS', 'JavaScript', 'REST', 'Node.js'
+      ]
+    }
+  ],
+  projects: [
+    {
+      title: 'Miami Heat',
+      duration: '2018-2021',
+      link: 'https://play.google.com/store/apps/details?id=com.xcosoftware.miamiheat',
+      desc: `The official app of the Miami HEAT basketball team with exclusive content, scores and schedules & more written in Java/Kotlin`
+    },
+    {
+      title: 'Atlanta Hawks',
+      duration: '2018-2021',
+      link: 'https://play.google.com/store/apps/details?id=com.yinzcam.nba.hawks',
+      desc: `Official mobile app of the Atlanta Hawks & State Farm Arena with exclusive content, scores and schedules & more written in Java/Kotlin`
+    },
+    {
+      title: 'Sacramento Kings',
+      duration: '2018-2021',
+      link: 'https://play.google.com/store/apps/details?id=com.kings',
+      desc: `Sacramento Kings + Golden 1 Center app, providing the ultimate interactive fan experience with exclusive content, scores and schedules & more written in Java/Kotlin`
+    },
+    {
+      title: 'Intelesafe',
+      duration: '2021-2021',
+      link: 'https://play.google.com/store/apps/details?id=org.intelehealth.intelesafe',
+      desc: `Developed to provide vetted information to help healthcare providers stay safe while being on the job`
+    },
+    {
+      title: 'iRobokid',
+      duration: '2019-2021',
+      link: 'https://play.google.com/store/apps/details?id=com.raweng.irobokid',
+      desc: `App for parents with access content like children's class updates, schedules, provide feedback & more written in Kotlin`
+    },
+    {
+      title: 'NBA OPiN SDK',
+      duration: '2017-2021',
+      link: 'https://play.google.com/store/apps/details?id=com.nbaimd.gametime.nba2011',
+      desc: `Partner Integration Network for NBA App written in Java/Kotlin`
+    },
+    {
+      title: 'Dell EMC Conference App',
+      duration: '2016-2017',
+      link: '',
+      desc: `Official mobile app of the Dell Dell EMC Conference with access to personal schedules, the conference agenda,
+      sponsor/exhibitor profiles, session information, social media & more written in React Native`
+    },
+    {
+      title: 'VMWorld Conference App - 2015/16',
+      duration: '2015-2016',
+      link: '',
+      desc: `Official mobile app of the VMWorld Conference with access to personal schedules, the conference agenda,
+      sponsor/exhibitor profiles, session information, social media & more written in Xamarin / React Native`
+    },
+    {
+      title: 'Built.io Backend SDK',
+      duration: '2013-2015',
+      link: '',
+      desc: `Enterprise Mobile Backend-as-a-Service SDK written in C#, Xamarin`
+    },
+    {
+      title: 'FinCore',
+      duration: '2012-2013',
+      link: 'https://www.finacus.co.in/fincore',
+      desc: ` fully browser-based and modular Core Banking System geared to resolve the challenges faced by Banks on a day to day basis`
+    },
+  ],
+  experiences: [
+    {
+      organization: 'Intelehealth',
+      title: `Sr. Android Engineer`,
+      date: '2021-Current',
+      details: [
+        `Worked on multiple healthcare apps`,
+        `Managed/Collaborated with other team members on design & functionality`,
+        `Added new features and fixed bugs in the apps`,
+        `Used Kotlin/Java to create new app features`,
+      ]
+    },
+    {
+      organization: 'Raw Engineering LLC',
+      title: `Sr. Application Engineer`,
+      date: '2017-2021',
+      details: [
+        `Worked on multiple apps and SDKs`,
+        `Collaborated with other team members on design & functionality`,
+        `Added new features and fixed bugs in the apps and SDKs`,
+        `Used Kotlin/Java to create new app features`,
+      ]
+    },
+    {
+      organization: 'Raw Engineering LLC',
+      title: `Application Engineer`,
+      date: '2013-2017',
+      details: [
+        `Worked on multiple apps and SDKs`,
+        `Collaborated with other team members on design & functionality`,
+        `Added new features and fixed bugs in the apps and SDKs`,
+        `Used Java to create new app features`,
+      ]
+    },
+    {
+      organization: 'Finacus Solutions Pvt. Ltd.',
+      title: `Software Engineer`,
+      date: '2012-2013',
+      details: [
+        `Worked on the Core Banking Application`,
+        `Added new features and fixed bugs in the app`,
+        `Collaborated with other team members on design & functionality`,
+        `Used C# / .Net Framework to create new app features`
+      ]
+    }
+  ],
+  certifications: [
+    {
+      desc: `Aptech Certified Computer Professional`,
+      date: '2012'
+    }
+  ],
+  events: []
+};
